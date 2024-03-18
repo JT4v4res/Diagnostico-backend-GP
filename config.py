6ee996b40b2d0607fce69530ab673f834edc654e
Gabriel Nicácio do Nascimento <60157992+nic4cio@users.noreply.github.com>
2024-03-18 16:03:05 -0300

    Project Initial Structure

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:password@db:5432/tasks'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
